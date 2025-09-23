b6171e7f0ea46763df4b0ca93b9e054a7e7edd07
Master Developments <finalcircuit.game@gmail.com>
2025-09-23 01:45:13 +0000

    server.ts:13   Server  Error: Route "/" used `cookies().get('sb-mjnwgeu

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,12 +1,10 @@
-import { createServerClient, type CookieOptions } from '@supabase/ssr'
-import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
-  let response = NextResponse.next({
-    request: {
-      headers: request.headers,
-    },
-  })
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
 
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -14,48 +12,28 @@ export async function updateSession(request: NextRequest) {
     {
       cookies: {
         get(name: string) {
-          return request.cookies.get(name)?.value
+          return request.cookies.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
-          request.cookies.set({
+          supabaseResponse.cookies.set({
             name,
             value,
             ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value,
-            ...options,
-          })
+          });
         },
         remove(name: string, options: CookieOptions) {
-          request.cookies.set({
+          supabaseResponse.cookies.set({
             name,
             value: '',
             ...options,
-          })
-          response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          })
-          response.cookies.set({
-            name,
-            value: '',
-            ...options,
-          })
+          });
         },
       },
     }
-  )
+  );
 
   // refreshing the session before loading server components
-  await supabase.auth.getUser()
+  await supabase.auth.getUser();
 
-  return response
+  return supabaseResponse;
 }
