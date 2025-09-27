8d916c7ec3e4c710416400d2e46235c0331822e7
Master Developments <finalcircuit.game@gmail.com>
2025-09-27 23:46:25 +0000

    oh wow, it DIDNT work. 404, 404, 404!!!!!!!!!!!!!!!!!!!!!

--- src/lib/env.ts
+++
@@ -1,6 +0,0 @@
-
-import { config } from 'dotenv';
-
-// Load environment variables from a .env file into process.env
-// This should be the first import in any server-side entry point.
-config({ path: '.env' });
