7f7e8febdf4f229f00d1dcba6351c3d02e98e15c
Master Developments <finalcircuit.game@gmail.com>
2025-09-28 01:39:38 +0000

    in firebase studio the app folder should be in root?

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,10 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   images: {
     remotePatterns: [
