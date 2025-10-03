c7a6d73b9ccb74950bfdf49696ae2a4efb6903bf
Master Developments <finalcircuit.game@gmail.com>
2025-10-03 02:29:38 +0000

    AlertDialogTrigger is not defined
    src/components/app/user-management.ts

--- src/components/app/user-management.tsx
+++ src/components/app/user-management.tsx
@@ -24,6 +24,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog';
 import { deleteUser } from '@/app/(auth)/actions/user';
 import { useToast } from '@/hooks/use-toast';
